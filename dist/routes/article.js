73950477bb8a5c0d77b4154428f7c83a9ffcea3e
Yewbdar <Yewbdar.grima@gmail.com>
2018-07-15 17:43:20 -0500

    12:27 Jul 15

--- dist/routes/article.js
+++ dist/routes/article.js
@@ -1,6 +1,5 @@
-const articlecontroller = require('./../controllers/article.ctrl')
-const multipart = require('connect-multiparty')
-const multipartWare = multipart()
+const articlecontroller = require('./../controllers/article.ctrl');
+
 
 module.exports = (router) => {
 
@@ -9,33 +8,6 @@ module.exports = (router) => {
      */
     router
         .route('/articles')
-        .get(articlecontroller.getAll)
+        .get(articlecontroller.getAll);
 
-    /**
-     * add an article
-     */
-    router
-        .route('/article')
-        .post(multipartWare, articlecontroller.addArticle)
-
-    /**
-     * clap on an article
-     */
-    router
-        .route('/article/clap')
-        .post(articlecontroller.clapArticle)
-
-    /**
-     * comment on an article
-     */
-    router
-        .route('/article/comment')
-        .post(articlecontroller.commentArticle)
-
-    /**
-     * get a particlular article to view
-     */
-    router
-        .route('/article/:id')
-        .get(articlecontroller.getArticle)
 }
