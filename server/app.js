cf9b0caa8f83e136c87d305f8d10979c4d1f0f65
Yewbdar <Yewbdar.grima@gmail.com>
2018-07-16 13:07:08 -0500

    folder remove

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 var session = require('express-session');
-
+const path = require("path");
 const app = express();
 const router = express.Router();
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/hp";
